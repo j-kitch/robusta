2bfa2fc05be879952a8e3ea9d1ab4394a28cdcb5
Joshua Kitchen <me@josh.kitchen>
2021-12-30 14:42:01 +0000

    Refactored local_vars

--- src/thread/local_vars.rs
+++ src/thread/local_vars.rs
@@ -7,41 +7,40 @@ impl LocalVars {
         LocalVars { bytes: vec![0; max_vars as usize * 4] }
     }
 
-    pub fn store_ref(&mut self, idx: u16, value: u32) {
-        let bytes: [u8; 4] = value.to_be_bytes();
-        for i in 0..4 {
-            let idx = (idx * 4) + i;
-            let idx = idx as usize;
-
-            self.bytes[idx] = bytes[i as usize];
+    fn store_bytes(&mut self, idx: u16, bytes: &[u8]) {
+        let mut idx = idx as usize;
+        for byte in bytes {
+            self.bytes[idx] = byte.clone();
+            idx += 1;
         }
+    }
+
+    fn load_word(&self, idx: u16) -> [u8; 4] {
+        let idx = idx as usize;
+        let mut bytes = [0; 4];
+        for i in 0..4 {
+            bytes[i]  = self.bytes[idx + i];
+        }
+        bytes
+    }
+
+    pub fn store_ref(&mut self, idx: u16, value: u32) {
+        let bytes = value.to_be_bytes();
+        self.store_bytes(idx, &bytes)
     }
 
     pub fn store_int(&mut self, idx: u16, value: i32) {
-        let bytes: [u8; 4] = value.to_be_bytes();
-        for i in 0..4 {
-            let idx = (idx * 4) + i;
-            let idx = idx as usize;
-
-            self.bytes[idx] = bytes[i as usize];
-        }
+        let bytes = value.to_be_bytes();
+        self.store_bytes(idx, &bytes)
     }
 
     pub fn load_ref(&self, idx: u16) -> u32 {
-        let idx = (idx * 4) as usize;
-        let mut bytes = [0; 4];
-        for i in 0..4 {
-            bytes[i] = self.bytes[idx + i];
-        }
-        u32::from_be_bytes(bytes)
+        let word = self.load_word(idx);
+        u32::from_be_bytes(word)
     }
 
     pub fn load_int(&self, idx: u16) -> i32 {
-        let idx = (idx * 4) as usize;
-        let mut bytes = [0; 4];
-        for i in 0..4 {
-            bytes[i] = self.bytes[idx + i];
-        }
-        i32::from_be_bytes(bytes)
+        let word = self.load_word(idx);
+        i32::from_be_bytes(word)
     }
 }
