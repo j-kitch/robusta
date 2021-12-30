77b5b9570045e07dfe19896e8bd4dd263a16d5fb
Joshua Kitchen <me@josh.kitchen>
2021-12-30 16:10:37 +0000

    Updated to 0.2.0, and added additional Robusta methods for bootstrapping

--- java/Robusta.java
+++ java/Robusta.java
@@ -5,4 +5,18 @@ public class Robusta {
     }
 
     public static native void println(String string);
+
+    public static native byte parseByte(String string);
+
+    public static native short parseShort(String string);
+
+    public static native int parseInt(String string);
+
+    public static native long parseLong(String string);
+
+    public static native char parseChar(String string);
+
+    public static native float parseFloat(String string);
+
+    public static native double parseDouble(String string);
 }
