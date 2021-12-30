b217b30007d598b7ac0ca9d0bd302a0b20a2e3bf
Joshua Kitchen <me@josh.kitchen>
2021-12-30 17:03:04 +0000

    Added multiplication

--- java/PrimitiveOps.java
+++ java/PrimitiveOps.java
@@ -2,6 +2,13 @@ public class PrimitiveOps {
 
     public static void main(String[] args) {
         int i = Robusta.parseInt(args[0]);
+
+        intOperations(i);
+    }
+
+    private static void intOperations(int i) {
+        Robusta.println(i);
+        i *= 54326;
         Robusta.println(i);
     }
 }
