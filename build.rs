094f8b56220c280e99d3812c1acf686795ed223b
Joshua Kitchen <me@josh.kitchen>
2022-03-11 09:50:53 +0000

    Configured build script to reduce build times

--- build.rs
+++ build.rs
@@ -1,16 +1,33 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
-    build_java_library(Path::new("robusta-java-runtime"));
-    build_java_library(Path::new("robusta-java-test"));
+    let java_libraries = ["robusta-java-runtime", "robusta-java-test"];
+
+    for java_library in java_libraries {
+        build_java_library(Path::new(java_library));
+    }
 }
 
 fn build_java_library(path: &Path) {
-    println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+    if cfg!(debug_assertions) {
+        // Conditionally rebuild only if we have changed src, pom or target dir.
+        let to_watch = [
+            PathBuf::from("src"),
+            PathBuf::from("pom.xml"),
+            PathBuf::from("target").join(path.with_extension("jar")),
+        ];
+
+        for child in to_watch {
+            println!("cargo:rerun-if-changed={}", path.join(child).to_str().unwrap());
+        }
+    }
+
+    let args = if cfg!(debug_assertions) { "install" } else { "clean install" };
+
     Command::new("mvn")
         .current_dir(path)
-        .args("clean install".split_whitespace())
+        .args(args.split_whitespace())
         .status()
         .unwrap();
 }
