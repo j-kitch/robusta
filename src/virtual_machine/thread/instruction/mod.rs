fce47d4ab767887a3b9c17f4ff9e26ececa17c78
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-03-01 21:21:28 +0000

    Added astore_<n> operations

--- src/virtual_machine/thread/instruction/mod.rs
+++ src/virtual_machine/thread/instruction/mod.rs
@@ -1,4 +1,4 @@
-use crate::java::Value;
+use crate::java::{CategoryOne, Value};
 use crate::virtual_machine::runtime::Const;
 use crate::virtual_machine::thread::Thread;
 
@@ -21,6 +21,21 @@ pub fn load_constant(thread: &mut Thread) {
         }
         _ => panic!("unsupported operation")
     }
+}
+
+/// astore_<n>
+///
+/// See [the spec](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.astore_n).
+pub fn astore_n(thread: &mut Thread, n: u16) {
+    let mut cur_frame = thread.stack.last_mut().unwrap();
+
+    let cat1 = match cur_frame.operand_stack.pop() {
+        Value::Reference(reference) => CategoryOne { reference },
+        Value::ReturnAddress(return_address) => CategoryOne { return_address },
+        _ => panic!("unsupported operation")
+    };
+
+    cur_frame.local_vars.store_cat1(n, cat1);
 }
 
 /// Instruction `return`
--- src/virtual_machine/thread/mod.rs
+++ src/virtual_machine/thread/mod.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::sync::Arc;
-use crate::java::Value;
+use crate::java::{CategoryOne, Value};
 
 use crate::virtual_machine::runtime::{ConstPool, Method, Runtime};
-use crate::virtual_machine::thread::instruction::{load_constant, r#return};
+use crate::virtual_machine::thread::instruction::{astore_n, load_constant, r#return};
 
 mod instruction;
 
@@ -25,6 +26,7 @@ impl Thread {
                 Frame {
                     const_pool: pool,
                     operand_stack: OperandStack::new(),
+                    local_vars: LocalVars::new(),
                     method,
                     pc: 0
                 }
@@ -46,6 +48,10 @@ impl Thread {
 
         match opcode {
             0x12 => load_constant(self),
+            0x4B => astore_n(self, 0),
+            0x4C => astore_n(self, 1),
+            0x4D => astore_n(self, 2),
+            0x4E => astore_n(self, 3),
             0xB1 => r#return(self),
             _ => panic!("not implemented opcode {:0x?}", opcode)
         }
@@ -59,6 +65,7 @@ pub struct Frame {
     /// A reference to the related method.
     method: Arc<Method>,
     operand_stack: OperandStack,
+    local_vars: LocalVars,
     /// The program counter within the current method.
     pc: usize,
 }
@@ -77,5 +84,28 @@ impl OperandStack {
 
     pub fn push(&mut self, value: Value) {
         self.stack.push(value);
+    }
+
+    pub fn pop(&mut self) -> Value {
+        self.stack.pop().unwrap()
+    }
+}
+
+/// Each frame contains an array of variables called the local variables.
+///
+/// For further information, see [the spec](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.6.1).
+pub struct LocalVars {
+    map: HashMap<u16, Value>
+}
+
+impl LocalVars {
+    pub fn new() -> Self {
+        LocalVars { map: HashMap::new() }
+    }
+
+    /// Store a value of category 1 (not a long or double) at the given position.
+    pub fn store_cat1(&mut self, index: u16, value: CategoryOne) {
+        let value = unsafe { value.int };
+        self.map.insert(index, Value::Int(value));
     }
 }
