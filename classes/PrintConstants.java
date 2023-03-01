29e6cfe8e9ed6bb4396db04e6385b3b1a4f958a0
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-03-01 17:30:34 +0000

    Added temp standard library and println of int/string constants

---
+++ classes/PrintConstants.java
@@ -0,0 +1,10 @@
+public class PrintConstants {
+
+    public static void main(String[] args) {
+        String string = "hello world";
+        int i = 542354326;
+
+        Robusta.println(i);
+        Robusta.println(string);
+    }
+}
---
+++ classes/Robusta.java
@@ -0,0 +1,6 @@
+public class Robusta {
+
+    public static native void println(String string);
+
+    public static native void println(int i);
+}
