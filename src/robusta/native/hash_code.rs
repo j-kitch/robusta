ef5b3f8dc408ca7ba958e7eb6a651d38aac614af
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-03-04 14:57:53 +0000

    Made hash code values repeatable

--- src/robusta/native/hash_code.rs
+++ src/robusta/native/hash_code.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 
-use rand::{Rng, thread_rng};
+use rand::{Rng, SeedableRng, thread_rng};
+use rand::prelude::ThreadRng;
+use rand::rngs::StdRng;
 
 use crate::java::{Int, MethodType, Reference, Value};
 use crate::native::{Args, Method, Plugin};
 
+const RNG_SEED: u64 = 542364236435;
+
 pub fn hash_code_plugins() -> Vec<Box<dyn Plugin>> {
-    let state = Arc::new(State { hash_codes: RwLock::new(HashMap::new()) });
+    let state = Arc::new(State { hash_codes: RwLock::new(HashMap::new()), rng: Mutex::new(StdRng::seed_from_u64(RNG_SEED)) });
 
     let get_hash_code: Box<dyn Plugin> = Box::new(GetHashCode { library: state.clone() }) as _;
     let register_hash_code: Box<dyn Plugin> = Box::new(RegisterHashCode { library: state.clone() }) as _;
@@ -18,6 +22,8 @@ pub fn hash_code_plugins() -> Vec<Box<dyn Plugin>> {
 /// The shared state for hash code functions.
 struct State {
     hash_codes: RwLock<HashMap<Reference, Int>>,
+    /// Using [`StdRng`] to get repeatable hash codes.
+    rng: Mutex<StdRng>,
 }
 
 struct GetHashCode {
@@ -55,7 +61,9 @@ impl Plugin for RegisterHashCode {
 
     fn call(&self, _: &Method, args: &Args) -> Option<Value> {
         let reference = args.params[0].reference();
-        let int: Int = Int(thread_rng().gen());
+
+        let int: i32 = self.library.rng.lock().unwrap().gen();
+        let int: Int = Int(int);
 
         let mut codes = self.library.hash_codes.write().unwrap();
         codes.insert(reference, int);
