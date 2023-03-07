320ed4c291052d1059f1ce4617f59211fb6fd63a
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-03-07 15:07:33 +0000

    Fixed const pool

--- src/robusta/method_area/const_pool.rs
+++ src/robusta/method_area/const_pool.rs
@@ -2,7 +2,9 @@ use std::collections::HashMap;
 use std::ops::Deref;
 use std::pin::Pin;
 use std::ptr::NonNull;
+use std::sync::Arc;
 use crate::class_file::ClassFile;
+use crate::class_file::const_pool as cp;
 
 use crate::collection::once::{Once, OnceMap};
 use crate::java::{FieldType, MethodType, Reference};
@@ -15,8 +17,76 @@ pub struct ConstPool {
 }
 
 impl ConstPool {
-    pub fn new(class_file: &ClassFile) -> Self {
-        todo!()
+    pub fn new(file: &ClassFile) -> Self {
+        let pool: HashMap<u16, Const> = HashMap::new();
+        let mut pool = ConstPool { pool };
+
+        // Want to descend keys to ensure that when we visit references to other constants,
+        // that those constants have already been added.
+        let mut keys: Vec<(&u16, &crate::class_file::const_pool::Const)> = file.const_pool.iter().collect();
+        keys.sort_by_key(|(_, con)| con.order());
+
+        for (key, _) in keys {
+            let val = file.const_pool.get(&key).unwrap();
+            match val {
+                cp::Const::Integer(integer) => {
+                    pool.pool.insert(*key, Const::Integer(integer.int));
+                }
+                cp::Const::String(string) => {
+                    let string = file.get_const_utf8(string.string);
+                    let string = String::from_utf8(string.bytes.clone()).unwrap();
+                    pool.pool.insert(*key, Const::String(SymbolicReference {
+                        const_key: string.to_string(),
+                        resolved: Once::new(),
+                    }));
+                }
+                cp::Const::Class(class) => {
+                    let name = file.get_const_utf8(class.name);
+                    let name = String::from_utf8(name.bytes.clone()).unwrap()
+                        .replace("/", ".");
+                    pool.pool.insert(*key, Const::Class(SymbolicReference {
+                        const_key: ClassKey { name: name },
+                        resolved: Once::new(),
+                    }));
+                }
+                cp::Const::FieldRef(field) => {
+                    let class = file.get_const_class(field.class);
+                    let class_name = file.get_const_utf8(class.name);
+                    let name_and_type = file.get_const_name_and_type(field.name_and_type);
+                    let name = file.get_const_utf8(name_and_type.name);
+                    let name = String::from_utf8(name.bytes.clone()).unwrap();
+                    let descriptor = file.get_const_utf8(name_and_type.descriptor);
+                    let descriptor = FieldType::from_descriptor(String::from_utf8(descriptor.bytes.clone()).unwrap().as_str()).unwrap();
+                    pool.pool.insert(*key, Const::Field(SymbolicReference {
+                        const_key: FieldKey {
+                            class: String::from_utf8(class_name.bytes.clone()).unwrap().replace("/", "."),
+                            name,
+                            descriptor
+                        },
+                        resolved: Once::new(),
+                    }));
+                }
+                cp::Const::MethodRef(method) => {
+                    let class = file.get_const_class(method.class);
+                    let class_name = file.get_const_utf8(class.name);
+                    let name_and_type = file.get_const_name_and_type(method.name_and_type);
+                    let name = file.get_const_utf8(name_and_type.name);
+                    let name = String::from_utf8(name.bytes.clone()).unwrap();
+                    let descriptor = file.get_const_utf8(name_and_type.descriptor);
+                    let descriptor = MethodType::from_descriptor(String::from_utf8(descriptor.bytes.clone()).unwrap().as_str()).unwrap();
+                    pool.pool.insert(*key, Const::Method(SymbolicReference {
+                        const_key: MethodKey {
+                            class: String::from_utf8(class_name.bytes.clone()).unwrap().replace("/", "."),
+                            name,
+                            descriptor
+                        },
+                        resolved: Once::new(),
+                    }));
+                }
+                _ => {}
+            }
+        }
+        pool
     }
 
     pub fn get_const(&self, index: u16) -> &Const {
